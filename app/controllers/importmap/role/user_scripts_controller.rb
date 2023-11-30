5e45354aa7fc34f61ebd1a8fe48a472696a5bb03
ryankopf <git@ryankopf.com>
2023-11-29 18:28:26 -0600

    Update

---
+++ app/controllers/importmap/role/user_scripts_controller.rb
@@ -0,0 +1,23 @@
+module Importmap
+  module Role
+    class UserScriptsController < ApplicationController
+      def index
+        combined_importmap = Importmap::Map.new
+    
+        Dir.glob(Rails.root.join('config/importmaps/*.rb')).each do |file_path|
+          role_name = File.basename(file_path, '.rb')
+          
+          if current_user.respond_to?("#{role_name}?") && current_user.public_send("#{role_name}?")
+            importmap = Importmap::Map.new
+            importmap.instance_eval(File.read(file_path))
+            combined_importmap.draw do
+              importmap.entries.each { |key, value| pin(key, to: value[:to], preload: value[:preload]) }
+            end
+          end
+        end
+    
+        render json: combined_importmap.to_json
+      end
+    end
+  end
+end
