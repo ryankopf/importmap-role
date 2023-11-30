778a1106a24957d61dd6bf30cc553c5b7b28714d
ryankopf <git@ryankopf.com>
2023-11-29 18:58:40 -0600

    Fix importmap configuration

--- config/initializers/importmap.rb
+++ config/initializers/importmap.rb
@@ -1,3 +1,5 @@
-config.importmap.draw do
-  pin "userimports", to: "path/to/userimports.js"
+Importmap::Role::Engine.configure do
+  config.importmap.draw do
+    pin "userimports", to: "path/to/userimports.js"
+  end
 end
